7328a1b867d770b19e2514d5fa6bb70fe44b12e1
evandroynno <evandro.ynno@gmail.com>
2020-12-04 20:22:37 -0300

    init

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+function App() {
+	return (
+		<Routes/>
+	);
+}
+
+export default App;
