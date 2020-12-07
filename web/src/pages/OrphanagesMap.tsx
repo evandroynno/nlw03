2d86a7e7d75e674397f315dee7a539b4a3a2a759
evandroynno <evandro.ynno@gmail.com>
2020-12-07 20:39:41 -0300

    Conectando Front com back
    Ficar de olho em Orphanage e CreateOrphanag

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {FiPlus} from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
-
-import "leaflet/dist/leaflet.css"
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import '../styles/pages/orphanages-map.css';
+import '../styles/components/sidebar.css'
 import mapMakerImg from "../images/map-marker.svg";
+import mapIcon from '../utils/mapIcon';
+
 
 function OrfanagesMap(){
     return(
@@ -32,9 +34,21 @@ function OrfanagesMap(){
                 <TileLayer 
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_KEY}`}
                 />
+                <Marker 
+                    icon={mapIcon}
+                    position= {[-22.825687,-42.9903308]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
-            <Link to="#" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color='#fff'/>
             </Link>
         </div>
